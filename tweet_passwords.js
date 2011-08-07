a5666c99edbff90700102190bc5d2925be3bf105
pangratz <pangratz666@gmail.com>
2011-08-07 23:14:53 +0200

    added tweeting of generated password

--- tweet_passwords.js
+++ tweet_passwords.js
@@ -1,4 +1,9 @@
 var applescript = require('applescript');
+var Twitter = require('twitter');
+var fs = require("fs");
+
+var props = JSON.parse(fs.readFileSync('twitter_properties.json', 'utf8'));
+var twitter = new Twitter(props);
 
 var executeAppleScript = function(pwLength){
 	pwLength = pwLength || 18;
@@ -8,10 +13,29 @@ var executeAppleScript = function(pwLength){
 			// Something went wrong!
 		}
 		console.log('length: ' + pwLength + ' password = ' + rtn);
+		if (rtn) {
+			twitter.updateStatus(rtn, function(data){
+			});
+		}
+		
 		setTimeout(function(){
 			executeAppleScript(pwLength < 31 ? pwLength + 1 : 8);
-		}, 500);
+		}, 1000*60*5);
 	});
 };
 
-executeAppleScript(8);
+// get latest tweet character count
+var params = {
+	count: 1
+};
+twitter.getUserTimeline(params, function(json){
+	if (Array.isArray(json) && json.length > 0) {
+		var tweet = json[0];
+		var length = tweet.text.length + 1;
+		console.log('start generating passwords of length ' + length);
+		executeAppleScript(length);
+	} else {
+		console.log('unable to get latest tweet');
+		console.log(json);
+	}
+});
