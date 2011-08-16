b247f9c3eb7615333b10ae0df7efce21477ece95
pangratz <pangratz666@gmail.com>
2011-08-16 12:18:07 +0200

    added logging to TweetPasswordServlet

--- src/main/java/com/pangratz/memorablepw/servlet/TweetPasswordServlet.java
+++ src/main/java/com/pangratz/memorablepw/servlet/TweetPasswordServlet.java
@@ -1,6 +1,7 @@
 package com.pangratz.memorablepw.servlet;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,7 @@ import com.pangratz.memorablepw.model.Password;
 
 public class TweetPasswordServlet extends HttpServlet {
 
+	private static final Logger log = Logger.getLogger(TweetPasswordServlet.class.getName());
 	private ModelUtils mModelUtils;
 
 	@Override
@@ -24,8 +26,11 @@ public class TweetPasswordServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Password pw = mModelUtils.getNextPassword(0, "en");
-		if (pw != null)
-			log("tweet password: " + pw.getText());
+		if (pw != null) {
+			log.info("tweet password: " + pw.getText());
+		} else {
+			log.info("no password available for lang en");
+		}
 		resp.flushBuffer();
 	}
 }
