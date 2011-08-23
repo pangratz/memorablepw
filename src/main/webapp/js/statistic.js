28d78ba3dc0cd1285a606c383536e1688091138c
pangratz <pangratz666@gmail.com>
2011-08-23 14:50:06 +0200

    added message when there are no more passwords to tweet

--- src/main/webapp/js/statistic.js
+++ src/main/webapp/js/statistic.js
@@ -7,13 +7,17 @@ $(document).ready(function() {
 		$.plot($('#graph'), [data]);
 		$('#overallPasswordsCount').html(statisticData.overallPasswordsCount + ' passwords');
 		
-		var days = statisticData.period.days;
-		var hours = statisticData.period.hours;
-		var minutes = statisticData.period.minutes;
-		var periodStr = days + (days == 1 ? ' day ' : ' days ');
-		periodStr += hours + (hours == 1 ? ' hour ' : ' hours ');
-		periodStr += minutes + (minutes== 1 ? ' minute' : ' minutes');
-		$('#lastTweetPeriod').html(periodStr);
+		var lastTweetDateHtml = 'no more passwords to tweet :(';
+		if (statisticData.overallPasswordsCount > 0) {
+			var days = statisticData.period.days;
+			var hours = statisticData.period.hours;
+			var minutes = statisticData.period.minutes;
+			var periodStr = days + (days == 1 ? ' day ' : ' days ');
+			periodStr += hours + (hours == 1 ? ' hour ' : ' hours ');
+			periodStr += minutes + (minutes== 1 ? ' minute' : ' minutes');
+			lastTweetDateHtml = periodStr;
+		}
+		$('#lastTweetPeriod').html(lastTweetDateHtml);
 	});
 	
 });
